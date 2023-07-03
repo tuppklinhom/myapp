34bcb82eebfdbde4aa0ab6f5778a8dc77317c535
Tupp <tupp.k@ku.th>
2023-07-03 11:55:21 +0700

    initial commit: add Cat

---
+++ src/myapp/Cat.java
@@ -0,0 +1,25 @@
+package myapp;
+
+import java.util.ArrayList;
+
+public class Cat {
+    String name;
+    int age;
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Cat(String name){
+        this.name = name;
+        age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
